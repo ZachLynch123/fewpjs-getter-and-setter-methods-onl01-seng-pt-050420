6c61e02be61a66bcc19b561c4bb922de9864989f
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-12-08 01:10:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 // Add your Circle class here
-
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  
+  get diameter() {
+    
+  }
+  
+  get circumference() {
+    
+  }
+  
+  get area() {
+    
+  }
+  
+}
 
 
 
